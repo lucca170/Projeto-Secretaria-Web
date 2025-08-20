7e69e52406a773f943024ef262892fa75bac6004
lucca170 <gugalucasilveira@gmail.com>
2025-08-20 08:58:09 -0300

    Commit inicial do projeto da secretaria

--- escola/pedagogico/urls.py
+++ escola/pedagogico/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    # Adicione suas views aqui, por exemplo:
-    # path('', sua_view, name='pedagogico_home'),
+    path('admin/', admin.site.urls),
+    # Adicione os URLs das suas outras aplicações aqui
+    path('', include('base.urls')), 
+    # Exemplo: path('pedagogico/', include('pedagogico.urls')),
 ]
