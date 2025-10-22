8097bd89a1836b9c4f47ff93a153100b512bbb85
lucca170 <gugalucasilveira@gmail.com>
2025-10-22 07:53:41 -0300

    indo arrumar o front

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-// src/main.jsx
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -14,15 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </ThemeProvider>
   </React.StrictMode>,
 )
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 0619855925e5aad0ec7553204e69828ab97532e1
